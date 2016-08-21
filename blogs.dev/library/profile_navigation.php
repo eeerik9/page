25d921cd06785d21c4185135468f2299f128db9a
Erik Lux <lux.erik@mailoo.org>
2016-08-21 14:52:16 +0200

    Updating eeerik9.org

--- blogs.dev/library/profile_navigation.php
+++ blogs.dev/library/profile_navigation.php
@@ -1,28 +1,23 @@
 <nav id="nav">
-	  <ul>
-          <h1> XXXXX Library </h1>
-          <b>Welcome : <i><?php echo $login_session; echo ' '; echo $nick_session; ?></i></b>
-          <br>
-          Books on yoga, conspiration theories, forbidden science, natural law, common law, inner psyche, true history of men, magic plants, sexuality, authority and much more ..
-          <br><br>
-          List of authors, list of inspirational websites, books to read accompanied by some art         
-          <br><br>
-          <br> 
-
-		<li class="border_top" <?php if ($thisPage=="authors") echo " id=\"currentpage\""; ?>>
-		  <a href="profile.php?page=authors.php">Authors</a>
-		</li>
-		<li class="border" <?php if ($thisPage=="websites") echo " id=\"currentpage\""; ?>>
-		  <a href="profile.php?page=websites.php">Websites</a>
-		</li>
-		<li class="border" <?php if ($thisPage=="books") echo " id=\"currentpage\""; ?>>
-		  <a href="profile.php?page=books.php">Books</a>
-		</li>
-		<li class="border" <?php if ($thisPage=="art") echo " id=\"currentpage\""; ?>>
-		  <a href="profile.php?page=art.php">Art</a>
-		</li>
-                <li class="border" <?php if ($thisPage=="logout") echo " id=\"currentpage\""; ?>>
-		  <a href="logout.php">Log Out</a>
-		</li>
-	  </ul>
+ <ul>
+  <h1> XXXXX Library </h1>
+  <b>Welcome : <i><?php echo $login_session; echo ' '; echo $nick_session; ?></i></b>
+  <br>
+  Information on yoga, conspiration theories, forbidden science, natural law, common law, inner psyche, true history of men, magic plants, sexuality, authority and much more ..
+  <br><br>
+  <br> 
+  <li class="border_top" <?php if ($thisPage=="authors") echo " id=\"currentpage\""; ?>>
+   <a href="profile.php?page=authors.php">Authors</a></li>
+  <li class="border" <?php if ($thisPage=="websites") echo " id=\"currentpage\""; ?>>
+   <a href="websites.php">Websites & Resources</a></li>
+  <li class="border" <?php if ($thisPage=="books") echo " id=\"currentpage\""; ?>>
+   <a href="profile.php?page=books.php">Books</a></li>
+  <li class="border" <?php if ($thisPage=="dictionary") echo " id=\"currentpage\""; ?>>
+   <a href="profile.php?page=dictionary.php">Dictionary</a></li>
+  <li class="border" <?php if ($thisPage=="art") echo " id=\"currentpage\""; ?>>
+   <a href="profile.php?page=art.php">Art</a></li>
+  <li class="border" <?php if ($thisPage=="logout") echo " id=\"currentpage\""; ?>>
+   <a href="logout.php">Log Out</a>
+  </li>
+ </ul>
 </nav>
---
+++ blogs.dev/library/websites.php
@@ -0,0 +1,308 @@
+<a href="library.php">Back to Library </a>
+<h2>Natural, Life Energy, Orgone </h2>
+Wilhelm Reich<br>
+<br>
+<a href="http://www.wilhelmreichtrust.org/" > Wilhelm Reich Trust </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.wilhelmreichtrust.org/" >
+</iframe>
+</div>
+<br>
+<a href="library.php">Back to Library </a> </br></br>
+Alexander Lowen<br>
+<a href="http://www.lowenfoundation.org/" > Lowen Foundations </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.lowenfoundation.org/" >
+</iframe>
+</div>
+<br>
+<a href="library.php">Back to Library </a> </br></br>
+Eva Reich<br>
+James Demeo<br>
+<a href="http://www.orgonelab.org/" > James Demeo Research Lab </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.orgonelab.org/" >
+</iframe>
+</div>
+<br>
+<a href="library.php">Back to Library </a> </br></br>Bernd Senf<br>
+<a href="http://www.berndsenf.de/" > Bernd Senf </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.berndsenf.de/" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br></br>
+a href="http://www.fuckforforest.com" > Fuck For Forest </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.fuckforforest.com/" >
+</iframe>
+</div>
+<br>
+
+
+
+
+<a href="library.php">Back to Library </a> </br></br><h2>Living Water </h2>
+Victor Schauberger<br>
+<a href="http://schauberger.co.uk/"> Viktor Schauberger </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://schauberger.co.uk/" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br></br>Masaru Emoto<br>
+<a href="http://www.masaru-emoto.net/english/index.html"> Masaru Emoto </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.masaru-emoto.net/english/index.html" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br></br><h2>Metaphysics, Universe</h2>
+<a href="http://walter-russell.com/" > Cosmology </a>
+<div>
+<iframe src="http://walter-russell.com/" width="960" height="540"
+</ilayer>
+</iframe>
+</div>
+<br>
+
+<a href="http://www.walter-russell.de/" > Cosmology II </a>
+<div>
+<iframe src="http://www.walter-russell.de/" width="960" height="540">
+</ilayer>
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br></br><h2>Electrity, Magnetism, Gravity and Dialectricity </h2>
+<a href="http://free-energy.ws/" > Free energy, Alternative energy </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://free-energy.ws/" >
+</iframe>
+</div>
+<br>
+Peter Linderman<br>
+Eric P. Dollard<br>
+Nikola Tesla<br>
+James Clerk Maxwell<br>
+Proteus Steinmetz<br>
+J. J. Thomson<br>
+Konstantin Meyl<br>
+<a href="http://www.k-meyl.de/" > Elektromagnetische Umweltvertraglichkeit Potentialwirbel, Skalarwellen & alternative Energie </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.k-meyl.de/" >
+</iframe>
+</div>
+Paul Babcock<br>
+<br>
+
+
+<a href="library.php">Back to Library </a> </br></br><h2>The nature of reality </h2>
+David Icke<br>
+<a href="http://www.davidicke.com/" > David Icke </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.davidicke.com/" >
+</iframe>
+</div>
+<br>
+<a href="library.php">Back to Library </a> </br></br><h2>Truth, Natural Law, Inner and Outer, Occult </h2>
+Michael Tsarion<br>
+<a href="http://www.michaeltsarion.com/" > Michael Tsarion </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.michaeltsarion.com/" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Santos Bonacci<br>
+<a href="http://universaltruthschool.com/" >Syncretism </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://universaltruthschool.com/" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Matk Passio<br>
+<a href="http://www.whatonearthishappening.com/" >What on earth is happening? </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.whatonearthishappening.com/" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Jordan Maxwell<br>
+<a href="
+https://jordanmaxwellshow.com" >The Jordan Maxwell Show </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="
+https://jordanmaxwellshow.com" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Terrenz Mckenna & Dennis Mckenna<br>
+<a href="
+https://heffter.org" >Psychedelic Institute </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="
+https://heffter.org" >
+</iframe>
+</div>
+<br>
+
+
+<a href="library.php">Back to Library </a> </br>
+<h2>Dialectricity, Magnetism, Vortex Science, Free Energy, Unity </h2>
+Ken Lee Wheeler<br>
+Uncovering the Missing Secrets of Magnetism<br>
+<a href="https://www.youtube.com/watch?v=VWMu0cndKl4" >Uncovering the missing secrets of magnetism </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.youtube.com/embed/VWMu0cndKl4?autoplay=0" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Nassim Harramein<br>
+<a href="http://resonance.is/" >Resonance Project </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://resonance.is/" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Marco Rodin<br>
+<a href="http://vortexmath.webs.com/" >Vortex Math </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://vortexmath.webs.com/" >
+</iframe>
+</div>
+<a href="http://rense.com/rodinaerodynamics.htm" >Rodin Aerodynamics </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://rense.com/rodinaerodynamics.htm" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+
+Free Energy</br>
+<a href="http://www.free-energy-info.com/" >Free energy devices </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.free-energy-info.com/" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Re-movement</br>
+<a href="http://www.re-movement.eu/" >Re movement </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://re-movement.eu/" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Max Igan</br>
+CrowHouse</br>
+<a href="http://thecrowhouse.com" >The Crow House </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://thecrowhouse.com" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Veronica: of the Champman family</br>
+Strawman</br>
+<a href="http://www.yourstrawman.com" >Strawman </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.yourstrawman.com" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+<h2>Bridge between Biology and Spirituality</h2>
+
+Betty Shine</br>
+<a href="http://bettyshine.co.uk" >Betty Shine </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://bettyshine.co.uk" >
+</iframe>
+</div>
+<br>
+
+
+
+Bruce Lipton</br>
+<a href="http://www.brucelipton.com" >Biology of Belief </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.brucelipton.com" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Gregg Braden</br>
+<a href="http://www.greggbraden.com" >Gregg Braden </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.greggbraden.com" >
+</iframe>
+</div>
+<br>
+
+<a href="library.php">Back to Library </a> </br>
+Joe Dispenza</br>
+<a href="http://www.drjoedispenza.com" >Joe Dispenza </a>
+<div>
+<iframe width="960" height="540"
+<ilayer src="http://www.drjoedispenza.com" >
+</iframe>
+</div>
+<br>
+
+
+
+
+
+
+
+	
+	
